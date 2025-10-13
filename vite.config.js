bb325354d0ad1f4dc293c1275ef7798971f67c62
replitdiffacc-web <replitdiffacc@gmail.com>
2025-10-13 16:06:25 +0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,25 +8,15 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5000,
     strictPort: true,
-    hmr: {
-      clientPort: 443,
-    },
-    allowedHosts: [
-      '61ffc738-a154-4d1a-8bf8-19aba61e7dee-00-1fwsgklc4pgsm.sisko.replit.dev',
-      '28d30785-22ca-4e6f-91bc-85aedb7a0e75-00-2tk2ccqc0k8ge.sisko.repl.co',
-      '28d30785-22ca-4e6f-91bc-85aedb7a0e75-00-2tk2ccqc0k8ge.sisko.replit.dev',
-      '0f3257ed-64c5-4f34-ac8c-fcc8b29e041a-00-omhqoehsygio.pike.replit.dev',
-      '0f3257ed-64c5-4f34-ac8c-fcc8b29e041a-00-omhqoehsygio.pike.repl.co',
-    ],
+    hmr: { clientPort: 443 },
+    allowedHosts: true,
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-      }
+      '/api': { target: 'http://0.0.0.0:8000', changeOrigin: true }
     }
   },
   preview: {
     host: '0.0.0.0',
     port: 5000,
+    allowedHosts: true
   },
 })
